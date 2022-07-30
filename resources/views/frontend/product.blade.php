b4dfbdd1b08b3f2f5f46be154321a0d0b3621055
unknown <you@example.com>
2022-07-30 17:26:44 +0100

    design chnages

--- resources/views/frontend/product.blade.php
+++ resources/views/frontend/product.blade.php
@@ -238,7 +238,7 @@
                 </div>
             </div>
         </div>
-        <div class="row mt-xl-3 pt-xxl-0 mb-xxl-4 mt-3" id="description">
+        <div class="row mt-xl-4 pt-xxl-0 mb-xxl-4 mt-3" id="description">
             <div class="col-md-2">
                 <div class="description_heading">
                     description
@@ -291,19 +291,19 @@
              {{ $OrderIncludes->title }}
         </div>
 
-        <div class="row mt-md-0 mt-xl-5 align-items-center">
+        <div class="row mt-md-0 mt-xl-4 align-items-center">
                 <!-- <div class="col-md-6 col-lg-4 pe-4">
                     <div class="order-include-img">
                         <img src="{{ url('frontend/image/order-includes.png') }}" alt="">
                     </div>
                 </div> -->
-                <div class="col-md-12 col-lg-12 col-lg-12">
-                    <div class="order-includes-heading mb-lg-4 mb-2 px-3 mt-lg-3 mt-2 text-center text-lg-start d-none d-xl-block">
+                <div class="col-md-12 col-lg-12 col-lg-12 px-3 px-md-0 order-part">
+                    <div class="order-includes-heading mb-lg-4 mb-2 mt-lg-3 mt-2 px-xl-3 px-xxl-0 text-center text-lg-start d-none d-xl-block">
                         {{ $OrderIncludes->title }}
                     </div>
                     <div class="row mt-2 mt-md-0">
                         @foreach($OrderIncludes->orderincludesdata as $orderincludesdata)
-                        <div class="col-md-4 col-xxl-2 order-box-part mb-3 px-0 px-md-3">
+                        <div class="col-md-4 col-xxl-2 order-box-part mb-3 px-0 px-md-3 order-include-col">
                             <div class="order-box">
                             <span class="order-img d-block mb-2">
                                 <img src="{{ url('images/order_image/'.$orderincludesdata->image) }}" alt="">   
