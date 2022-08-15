11ed7e5141873c4949653ea6d11799e883f8d370
pankaj5604 <dev29@ezeness.com>
2022-08-15 10:19:16 +0530

    15-08-2022 working 2

--- resources/views/email/mailDatainquiry.blade.php
+++ resources/views/email/mailDatainquiry.blade.php
@@ -13,31 +13,26 @@
             <div style="text-align: center; margin-bottom: 20px;">
                 <img src="{{ url('frontend/image/logo-transparent.png') }}" alt="" style="display: block;margin-left: auto;margin-right: auto; width: 100px;">
             </div>
-            <div style="background-color: #ffffff;padding: 50px; min-width:650px; box-shadow: 0px 0px 4px 4px rgb(0 0 0 / 1%);margin-left: 100px; text-align: center;">
+            <div style="background-color: #ffffff;padding: 50px; min-width:650px; box-shadow: 0px 0px 4px 4px rgb(0 0 0 / 1%); margin-left: 100px; text-align: center;">
                 <div style="margin-bottom: 25px; font-weight: 900;font-size: 18px;">Thank you for inquiry</div>
-                <div style="margin-bottom: 25px;font-size: 13px;">
-                     Our Representative will contact you soon.
-                </div>
-                <div style="margin-bottom: 5px; font-size: 13px;">
-                    {!! $product_info !!} <br>
-                    {!! $diamond_info !!}  <br>
-                    {!! $spe_info !!}   <br>
-                </div>
+                    <div style="margin-bottom: 25px;font-size: 13px;">
+                        Our Representative will contact you soon.
+                    </div>
+                    <div style="margin-bottom: 5px; font-size: 13px;"> 
+                    </div>
                 
-                <!-- <button style="margin-bottom: 25px;color: #fff;background-color: #1a5db6; border: 0;padding: 13px 30px;border-radius: 5px; font-size: 13px;">Confirm email</button>
-                
-                <a href="#" style="color: #1a5db6;font-size: 13px;margin-bottom: 25px;display: inline-block;">https:/inklinkilinknikniinintaniininasdaniaisniasiadnal/basiéahskhda/ <br>
-                    askdaksdabskabaksbdakd</a> -->
-                <div style="margin-bottom: 5px; font-size: 13px;">
-                    Thank you!
-                    <div style="margin-top:3px;">Gemver Affordable Luxury Team</div>
+               
+                    <div style="margin-bottom: 5px; font-size: 13px;">
+                        Thank you!
+                        <div style="margin-top:3px;">Gemver Affordable Luxury Team</div>
+                    </div>
                 </div>
-            </div>
-            <div style="text-align: center; margin-top: 50px;color: #b7b7b7;font-size: 13px;">
-                © 2022 Gemver Affordable Luxury 
-            </div>
+                <div style="text-align: center; margin-top: 50px;color: #b7b7b7;font-size: 13px;">
+                    © 2022 Gemver Affordable Luxury 
+                </div>
+           </div>
         </div>
-    </div>
+    </div>    
 </body>
 
 </html>
