e5591988b0eced146314e7763600298935ae54e6
pankaj5604 <pankajahir631@gmail.com>
2022-09-16 12:43:06 +0530

    working 16-09-2022 6

--- resources/views/email/mailDataNewsLatter.blade.php
+++ resources/views/email/mailDataNewsLatter.blade.php
@@ -5,14 +5,22 @@
     <title></title>
 </head>
 <body style="background-color: #f5f5f5;">
-    <div style="margin: auto;display: flex; align-items: center; justify-content: center;margin-top: 50px;min-height: 100vh;">
-        <div style="font-family: 'Roboto', sans-serif; ">
+    <div style="margin: auto; align-items: center; justify-content: center;margin-top: 50px;min-height: 100vh;">
+        <div style="font-family: 'Roboto', sans-serif; margin-left:20px; ">
             <div style="text-align: center; margin-bottom: 20px;">
-                <img src="{{ url('frontend/image/logo-transparent.png') }}" alt="" width="100% " style="width: 300px; ">
+                <img src="{{ url('frontend/image/logo-transparent.png') }}" alt=""  style="width: 150px;">
             </div>
-            <div style="background-color: #fff;padding: 50px;min-width:650px;box-shadow: 0px 0px 4px 4px rgb(0 0 0 / 1%);">
-                <div style="margin-bottom: 25px; font-weight: 900;font-size: 18px;">{!! $message1 !!}</div>
+            
+            <div style="margin-left:100px;background-color: #fff; padding: 50px; min-width:650px;box-shadow: 0px 0px 4px 4px rgb(0 0 0 / 1%); ">
+                <div style="margin-bottom: 5px; font-size: 13px;">
+                     {!! $message1 !!}
+                </div>
+                <div style="margin-bottom: 5px; font-size: 13px;">
+                    Thank you!
+                    <div style="margin-top:3px;">Gemver Affordable Luxury Team</div>
+                </div>
             </div>
+          
             <div style="text-align: center; margin-top: 50px;color: #b7b7b7;font-size: 13px;">
                 © 2022 Gemver Affordable Luxury 
             </div>
