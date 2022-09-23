2d2451d6b0be08084f285d5f3b03125e55c321f1
unknown <you@example.com>
2022-09-23 18:10:06 +0100

    design changes

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -19,7 +19,7 @@ class ContactUsController extends Controller
     
     public function index(){
         $settings = Settings::first();
-        return view('frontend.Address',compact('settings'));
+        return view('frontend.Checkout',compact('settings'));
     }
 
     public function save(Request $request){
