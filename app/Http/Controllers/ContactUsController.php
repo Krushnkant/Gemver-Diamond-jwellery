b7716d562cd00f16e206b23024d0f7dad7d50b66
unknown <you@example.com>
2022-09-29 12:54:46 +0100

    design chnages

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -19,7 +19,7 @@ class ContactUsController extends Controller
     
     public function index(){
         $settings = Settings::first();
-        return view('frontend.forget_password',compact('settings'));
+        return view('frontend.',compact('settings'));
     }
 
     public function save(Request $request){
