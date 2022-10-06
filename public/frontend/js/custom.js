157d32486ad30172b731edc49473dbd6c9c2d9a5
unknown <you@example.com>
2022-10-06 17:38:51 +0100

    shop page in review box design

--- public/frontend/js/custom.js
+++ public/frontend/js/custom.js
@@ -20,6 +20,23 @@
          return false;
      });
 
+     $('.product-detail-modal-popup').owlCarousel({
+         loop: true,
+         margin: 10,
+         nav: true,
+         dots: false,
+         responsive: {
+             0: {
+                 items: 1
+             },
+             600: {
+                 items: 1
+             },
+             1000: {
+                 items: 1
+             }
+         }
+     });
      $('.shop-dimond-by-shape-slider').owlCarousel({
          loop: true,
          margin: 10,
@@ -375,6 +392,8 @@
      $(".home-page-slider > .owl-nav > .owl-prev").html('<svg xmlns="http://www.w3.org/2000/svg" width="9" class="" height="13" viewBox="0 0 9 13" fill="none"><path d="M0.632325 6.50016L6.89274 12.7606L8.3667 11.2887L3.57503 6.50016L8.3667 1.71266L6.89378 0.239746L0.632325 6.50016Z" fill="#fff"/></svg>');
      $(".products_item > .owl-nav > .owl-next").html('<svg xmlns="http://www.w3.org/2000/svg" width="9" class="" height="13" viewBox="0 0 9 13" fill="none"><path d="M8.36767 6.49984L2.10726 0.239422L0.633301 1.7113L5.42497 6.49984L0.633301 11.2873L2.10622 12.7603L8.36767 6.49984Z" fill="#fff"/></svg>');
      $(".products_item > .owl-nav > .owl-prev").html('<svg xmlns="http://www.w3.org/2000/svg" width="9" class="" height="13" viewBox="0 0 9 13" fill="none"><path d="M0.632325 6.50016L6.89274 12.7606L8.3667 11.2887L3.57503 6.50016L8.3667 1.71266L6.89378 0.239746L0.632325 6.50016Z" fill="#fff"/></svg>');
+     $(".product-detail-modal-popup > .owl-nav > .owl-next").html('<svg xmlns="http://www.w3.org/2000/svg" width="9" class="" height="13" viewBox="0 0 9 13" fill="none"><path d="M8.36767 6.49984L2.10726 0.239422L0.633301 1.7113L5.42497 6.49984L0.633301 11.2873L2.10622 12.7603L8.36767 6.49984Z" fill="#fff"/></svg>');
+     $(".product-detail-modal-popup > .owl-nav > .owl-prev").html('<svg xmlns="http://www.w3.org/2000/svg" width="9" class="" height="13" viewBox="0 0 9 13" fill="none"><path d="M0.632325 6.50016L6.89274 12.7606L8.3667 11.2887L3.57503 6.50016L8.3667 1.71266L6.89378 0.239746L0.632325 6.50016Z" fill="#fff"/></svg>');
  });
  $(window).scroll(function() {
      var sticky = $('.header-part'),
