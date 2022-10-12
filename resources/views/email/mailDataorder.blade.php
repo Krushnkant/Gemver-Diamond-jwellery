125c65b5d531c5bd9d20e9cbc0012624d0f98f34
pankaj5604 <pankajahir631@gmail.com>
2022-10-12 17:50:29 +0530

    working 12-10-2022 5

--- resources/views/email/mailDataorder.blade.php
+++ resources/views/email/mailDataorder.blade.php
@@ -1,11 +1,11 @@
 <?php 
 $SupplierUrl = "demo";
-$OrderId = "demo";
+
 $CompanyAddress = "a101, city center, laldarwaja, varachha, surat, gujarat 395003";
 $SupplierName = "demo";
-$CustomerFullAddr = "demo";
+
 $SubOrderId = "demo";
-$CustomerName = "Pankaj";
+
 
  $Refmsg = '<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta name="x-apple-disable-message-reformatting"><meta http-equiv="X-UA-Compatible" content="IE=edge"><title></title><style type="text/css">a { color: #0000ee; text-decoration: underline; } .clean-body table{ width: 100%; } [owa] .u-row .u-col {display: table-cell;float: none !important;vertical-align: top;.ie-container .u-row,[owa] .u-row {width: 600px !important;}.ie-container .u-col-100,[owa] .u-col-100 {width: 600px !important;}@media only screen and (min-width: 620px) {.u-row {width: 600px !important;}.u-row .u-col {vertical-align: top;}.u-row .u-col-100 {width: 600px !important;}}@media (max-width: 620px) {.u-row-container {max-width: 100% !important;padding-left: 0px !important;padding-right: 0px !important;}.u-row .u-col {min-width: 320px !important;max-width: 100% !important;display: block !important;}.u-row {width: calc(100% - 40px) !important;}.u-col {width: 100% !important;}.u-col > div {margin: 0 auto;}.no-stack .u-col {min-width: 0 !important;display: table-cell !important;}.no-stack .u-col-100 {width: 100% !important;}}body {margin: 0;padding: 0;}table,tr,td {vertical-align: top;border-collapse: collapse;}p {margin: 0;}.ie-container table,.mso-container table {table-layout: fixed;}* {line-height: inherit;}a[x-apple-data-detectors="true"] {color: inherit !important;text-decoration: none !important;}.ExternalClass,.ExternalClass p,.ExternalClass span,.ExternalClass font,.ExternalClass td,.ExternalClass div {line-height: 100%;}@media (max-width: 480px) {.hide-mobile {display: none !important;max-height: 0px;overflow: hidden;}}@media (min-width: 481px) {.hide-desktop {display: none !important;max-height: none !important;}}</style><link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css"></head><body class="clean-body" style="margin: 0;padding: 0;-webkit-text-size-adjust: 100%;background-color: #ffffff">';
                 $Refmsg .= '<table class="nl-container" style="border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;min-width: 320px;Margin: 0 auto;background-color: #ffffff;width:100%" cellpadding="0" cellspacing="0">';
