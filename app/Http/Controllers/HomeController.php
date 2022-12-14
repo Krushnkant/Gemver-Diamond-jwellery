5dbfbe8ecd691b6d7230428be6e46fae9c0af821
Zahid Shaikh <zahid.shaikh.9013@gmail.com>
2022-12-14 10:03:04 +0530

    working 14-12-2022 1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -62,30 +62,6 @@ class HomeController extends Controller
     }
 
 
-    public function createSlug($title, $id = 0)
-    {
-        $slug = str_slug($title);
-        $allSlugs = $this->getRelatedSlugs($slug, $id);
-        if (! $allSlugs->contains('slug', $slug)){
-            return $slug;
-        }
-
-        $i = 1;
-        $is_contain = true;
-        do {
-            $newSlug = $slug . '-' . $i;
-            if (!$allSlugs->contains('slug', $newSlug)) {
-                $is_contain = false;
-                return $newSlug;
-            }
-            $i++;
-        } while ($is_contain);
-    }
-    protected function getRelatedSlugs($slug, $id = 0)
-    {
-        return ProductVariant::select('slug')->where('slug', 'like', $slug.'%')
-        ->where('id', '<>', $id)
-        ->get();
-    }
+    
     
 }
--- app/Http/Controllers/admin/BlogController.php
+++ app/Http/Controllers/admin/BlogController.php
@@ -76,6 +76,7 @@ class BlogController extends Controller
             }
 
             $blog->title = $request->title;
+            $blog->slug = $this->createSlug($request->title,$request->blog_id);
             $blog->description = $request->description;
             $blog->category_id = $request->category_id;
             $blog->meta_title = $request->meta_title;
@@ -85,6 +86,7 @@ class BlogController extends Controller
             $action = "add";
             $blog = new Blog();
             $blog->title = $request->title;
+            $blog->slug = $this->createSlug($request->title);
             $blog->created_at = new \DateTime(null, new \DateTimeZone('Asia/Kolkata'));
             $blog->blog_thumb = $request->catImg;
             $blog->description = $request->description;
@@ -272,5 +274,31 @@ class BlogController extends Controller
             @header('Content-type: text/html; charset=utf-8'); 
             echo $response;
         }
+    }
+
+    public function createSlug($title, $id = 0)
+    {
+        $slug = str_slug($title);
+        $allSlugs = $this->getRelatedSlugs($slug, $id);
+        if (! $allSlugs->contains('slug', $slug)){
+            return $slug;
+        }
+
+        $i = 1;
+        $is_contain = true;
+        do {
+            $newSlug = $slug . '-' . $i;
+            if (!$allSlugs->contains('slug', $newSlug)) {
+                $is_contain = false;
+                return $newSlug;
+            }
+            $i++;
+        } while ($is_contain);
+    }
+    protected function getRelatedSlugs($slug, $id = 0)
+    {
+        return Blog::select('slug')->where('slug', 'like', $slug.'%')
+        ->where('id', '<>', $id)
+        ->get();
     }
 }
