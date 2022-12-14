d67dfa5a6f2409d1b051d8117f582ce79d0ce392
Zahid Shaikh <zahid.shaikh.9013@gmail.com>
2022-12-14 09:20:40 +0530

    working 14-12-2022

--- app/Models/HomeSetting.php
+++ app/Models/HomeSetting.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class HomeSetting extends Model
 {
     use HasFactory;
+
+    public function category(){
+        return $this->hasOne(Category::class,'id','section_customise_link');
+    }
 }
