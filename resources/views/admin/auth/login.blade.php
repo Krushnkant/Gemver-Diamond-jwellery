3be4a9fb647f9f8af5bdfd65c17355c1872f5932
unknown <you@example.com>
2022-12-17 10:00:38 +0000

    design chnages

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,34 +1,58 @@
 @extends('admin.auth.layout')
 
 @section('content')
-    <div class="row justify-content-center h-100">
-        <div class="col-xl-6">
-            <div class="form-input-content">
-                <div class="card login-form mb-0">
-                    <div class="card-body pt-5">
-                        @if(\Illuminate\Support\Facades\Session::has('success'))
-                            <div class="alert alert-danger">
-                                {{ \Illuminate\Support\Facades\Session::get('success') }}
-                                @php
-                                    \Illuminate\Support\Facades\Session::forget('success');
-                                @endphp
-                            </div>
-                        @endif
 
-                        <h4>Gemver Admin</h4>
+    <div class="row admin-login-row">
+        <div class="col-xl-8 offset-xl-2 h-100 align-self-center">
+            <div class="admin-login-box card justify-content-center mb-0">
+                <div class="row">   
+                    <div class="col-lg-6 pr-lg-0">
+                        <div class="form-input-content">
+                            <div class="login-form mb-0 ">
+                                <div class="card-body pt-5 mt-5 admin-card-body">
+                                    @if(\Illuminate\Support\Facades\Session::has('success'))
+                                        <div class="alert alert-danger">
+                                            {{ \Illuminate\Support\Facades\Session::get('success') }}
+                                            @php
+                                                \Illuminate\Support\Facades\Session::forget('success');
+                                            @endphp
+                                        </div>
+                                    @endif
+                                   <div class="gemver-logo">
+                                        <img src="{{ asset('frontend/image/admin_banner/logo.png') }}">
+                                   </div>
+                                    <h4 class="create-account-heading">Admin Login</h4>
 
-                        <form method="post" action="" class="mt-5 mb-5 login-input" id="LoginForm">
-                            {{ csrf_field() }}
-                            <div class="form-group">
-                                <input type="text" class="form-control" id="email" name="email" placeholder="Email" value="{{ old('email') }}">
-                                <div id="email-error" class="invalid-feedback animated fadeInDown" style="display: none;"></div>
+                                    <form method="post" action="" class="mt-5 mb-5 login-input" id="LoginForm">
+                                        {{ csrf_field() }}
+                                        <div class="form-group custom-form-label">
+                                            <label for="email">Email</label>
+                                            <input type="text" class="form-control" id="email" name="email" placeholder="Email Id"  value="{{ old('email') }}" autocomplete="off">
+                                            <div id="email-error" class="invalid-feedback animated fadeInDown" style="display: none;"></div>
+                                        </div>
+                                        <div class="form-group custom-form-label">
+                                            <label for="password">Password</label>
+                                            <input type="password" class="form-control password-input" id="password" name="password" placeholder="Password" value="{{ old('password') }}">
+                                            <div id="password-error" class="invalid-feedback animated fadeInDown" style="display: none;"></div>
+                                        </div>
+                                        <button class="btn login-form__btn submit w-100 sign-up-btn" type="submit" id="loginSubmit">Sign Up <i class="fa fa-spinner fa-spin loadericonfa" style="display:none;"></i></button>
+                                    </form>
+                                </div>
                             </div>
-                            <div class="form-group">
-                                <input type="password" class="form-control" id="password" name="password" placeholder="Password" value="{{ old('password') }}">
-                                <div id="password-error" class="invalid-feedback animated fadeInDown" style="display: none;"></div>
-                            </div>
-                            <button class="btn login-form__btn submit w-100" type="submit" id="loginSubmit">Sign In <i class="fa fa-spinner fa-spin loadericonfa" style="display:none;"></i></button>
-                        </form>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 pl-lg-0">
+                        <div class="owl-carousel owl-theme signup-slider">
+                                <div class="item">
+                                    <img src="{{ asset('frontend/image/admin_banner/admin_banner_5.png') }}">
+                                </div>
+                                <div class="item">
+                                    <img src="{{ asset('frontend/image/admin_banner/admin_banner_1.png') }}">
+                                </div>
+                                <div class="item">
+                                    <img src="{{ asset('frontend/image/admin_banner/admin_banner_4.png') }}">
+                                </div>
+                        </div>
                     </div>
                 </div>
             </div>
