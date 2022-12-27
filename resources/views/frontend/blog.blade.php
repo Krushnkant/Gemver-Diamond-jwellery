85a5c06c626d1b0b3f88edd637edeb5d9159d6a5
pankaj5604 <pankajahir631@gmail.com>
2022-12-27 14:29:17 +0530

    working 27-12-2022 1

--- resources/views/frontend/blog.blade.php
+++ resources/views/frontend/blog.blade.php
@@ -4,14 +4,14 @@
             <div class="">
                 <!-- <img src="{{ asset('frontend/image/about_us.png') }}" alt=""> -->
                 <div class="about_us_background">
-                    <div class="sub_heading mb-lg-3">Blog detail page</div>
+                    <div class="sub_heading mb-lg-3">{{ $blog->title }}</div>
                     <div class="about_us_link">
                         <a href="{{ URL('/') }}">home</a>
                         <svg xmlns="http://www.w3.org/2000/svg" width="17" height="14" viewBox="0 0 17 14" fill="none" class="mx-2">
                             <path d="M4.30029 4.32471L6.97613 7L4.30029 9.67529L5.44971 10.8247L9.27388 7L5.44971 3.17529L4.30029 4.32471Z" fill="white"/>
                             <path d="M8.30029 4.32471L10.9761 7L8.30029 9.67529L9.44971 10.8247L13.2739 7L9.44971 3.17529L8.30029 4.32471Z" fill="white"/>
                         </svg>
-                        <a href="#">blog detail page</a>
+                        <a href="#">{{ $blog->title }}</a>
                     </div>
                 </div>
             </div>
