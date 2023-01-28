8555d12c5caadba43ed8b548fd2bc71dcf20b3ec
unknown <you@example.com>
2023-01-28 09:40:01 +0000

    design chages in gemver

--- resources/views/frontend/engagement.blade.php
+++ resources/views/frontend/engagement.blade.php
@@ -65,20 +65,20 @@
             Our Engagement Picks
         </div> 
        <div class="row">
-            <div class="col-8 image_view_column banner_part_img_parent first_image_part h-100 pe-0 ps-0 ps-md-3">
-                <figure class="position-relative mb-0 me-2">
+            <div class="col-12 col-md-8 image_view_column banner_part_img_parent first_image_part h-100 pe-0 ps-0 ps-md-3">
+                <figure class="position-relative mb-0 me-md-2">
                     <img src="{{ url('frontend/image/banner_3.jpg') }}" alt="asscher" class="fancy_color_img me-0">
                     <div class="lab_grown_diamonds_text our_engagement_label">Lab grown diamonds</div>
                 </figure>
             </div>
-            <div class="col-4 image_view_column_part banner_part_img_parent engagement_part px-0 px-md-3">
-                <figure class="position-relative mb-0"> 
+            <div class="col-12 col-md-4 image_view_column_part banner_part_img_parent engagement_part px-0 px-md-1 d-flex">
+                <figure class="position-relative mb-0 image_view_column_part_1"> 
                     <img src="{{ url('frontend/image/banner_3.jpg') }}" alt="asscher" class="fancy_color_img">
                     <div class="lab_grown_diamonds_text">Lab grown diamonds</div>
                 </figure>
                 
-                <figure class="mb-0 position-relative">
-                    <img src="{{ url('frontend/image/banner_3.jpg') }}" alt="asscher" class="fancy_color_img">
+                <figure class="mb-0 position-relative ms-2 image_view_column_part_2">
+                    <img src="{{ url('frontend/image/banner_3.jpg') }}" alt="asscher" class="fancy_color_img mt-0">
                     <div class="lab_grown_diamonds_text three-lab-grown">Lab grown diamonds</div>
                 </figure>
                 
