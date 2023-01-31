b26edd4666a4e1fbccb224a5052c48dd319da097
pankaj5604 <pankajahir631@gmail.com>
2023-01-31 12:56:36 +0530

    working 31-01-2023 9

--- app/View/Components/RelatedProduct.php
+++ app/View/Components/RelatedProduct.php
@@ -32,7 +32,12 @@ class RelatedProduct extends Component
     public function render()
     {
         $primary_category_idss = $this->categoryidss;
-        $ProductRelated= Product::select('products.id','products.product_title','products.primary_category_id','product_variants.slug','product_variants.alt_text','product_variants.images','product_variants.regular_price','product_variants.sale_price','product_variants.id as variant_id')->leftJoin("product_variants", "product_variants.product_id", "=", "products.id")->where(['products.is_custom' => 0,'products.estatus' => 1,'product_variants.estatus' => 1,'product_variants.term_item_id' => 2])->WhereIn('primary_category_id',$primary_category_idss)->where('products.id','<>',$this->productid)->groupBy('products.id')->limit(8)->get();
+        //$ProductRelated= Product::select('products.id','products.product_title','products.primary_category_id','product_variants.slug','product_variants.alt_text','product_variants.images','product_variants.regular_price','product_variants.sale_price','product_variants.id as variant_id')->leftJoin("product_variants", "product_variants.product_id", "=", "products.id")->where(['products.is_custom' => 0,'products.estatus' => 1,'product_variants.estatus' => 1,'product_variants.term_item_id' => 2])->WhereIn('primary_category_id',$primary_category_idss)->where('products.id','<>',$this->productid)->groupBy('products.id')->limit(8)->get();
+
+        $ProductRelated= Product::with('product_variant')->where(['is_custom' => 0,'estatus' => 1])->WhereHas('product_variant',function ($mainQuery) {
+            $mainQuery->where('term_item_id',2)->where('estatus',1);
+        })->WhereIn('primary_category_id',$primary_category_idss)->where('id','<>',$this->productid)->groupBy('id')->limit(8)->get();
+       
         return view('components.related-product',compact('ProductRelated'));
     }
 }
