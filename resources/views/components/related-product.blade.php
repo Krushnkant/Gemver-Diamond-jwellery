c0b12d4705a7b3b99abfa4d753c554d8f83c5c30
Krushnkant Vadadoriya <39893686+Krushnkant@users.noreply.github.com>
2023-02-11 19:18:09 +0530

    KK
    Product Details

--- resources/views/components/related-product.blade.php
+++ resources/views/components/related-product.blade.php
@@ -6,9 +6,6 @@
                     <div>
                         <h2 class="heading-h2 mb-xl-5 mb-3 mt-md-0">Related Product </h2>
                     </div>
-                    <!-- <div class="category-line-img d-none d-md-block">
-                        <img src="{{ asset('frontend/image/category-line.png') }}" alt="">
-                    </div> -->
                 </div>
                 <div class="owl-carousel owl-theme product-detail mb-5 px-0">
                     @foreach($ProductRelated as $key => $Related)
@@ -28,58 +25,66 @@
                     
                     ?>
                     <div class="hover_effect_part wire_bangle_shop_radio" >
-                    <div class="wire_bangle_img_radio_button">
-                        <div class="wire_bangle_img mb-3 position-relative">
-                            <a class="wire_bangle_hover_a" href="{{ $url }}"><img src="{{ $image }}" alt="{{ $alt_text }}"></a>
-                        </div>
-                        <div class="wire_bangle_description p-3"><div class="wire_bangle_heading mb-2">{{ $Related->primary_category->category_name }}
-                            <input type="hidden" class="variant_id" value="{{ $Related->product_variant[$key]->id }}">    
-                            <input type="hidden" class="item_type" value="0">    
-                            <span type="button" class="btn btn-default add-to-wishlist-btn" data-toggle="tooltip" data-placement="right" title="Wishlist">
-                                <?php if(is_wishlist($Related->product_variant[$key]->id,0)){ ?>
-                                    <i class="fas fa-heart heart-icon-part"></i>
-                                <?php }else{ ?>
-                                    <i class="far fa-heart"></i> 
-                                <?php }
-                                ?>
-                            </span>
-                        </div>
-                            <div class="wire_bangle_sub_heading wire_bangle_description"><a href="{{ $url }}">{{ $Related->product_title }}</a></div>
-                            <div class="d-flex justify-content-between pt-2 align-items-center">
-                                <div class="d-flex align-items-center">
-                                    <span class="wire_bangle_price wire_bangle_price_part">
-                                        $ {{ $sale_price }}
-                                    </span>
-                                    <span class="ms-2 wire_bangle_dublicate_price product_detail_regular_price">$<span class="regular_price1">{{ $regular_price }}</span></span>
-                                </div>
+                        <div class="wire_bangle_img_radio_button">
+                            <div class="wire_bangle_img mb-3 position-relative">
+                                <a class="wire_bangle_hover_a" href="{{ $url }}"><img src="{{ $image }}" alt="{{ $alt_text }}"></a>
+                            </div>
+                            <div class="wire_bangle_description p-2">
                                 <?php 
-                                $ProductVariantVariant = \App\Models\ProductVariantVariant::with('attribute','attribute_terms')->where('estatus',1)->where('product_id',$Related->id)->groupBy('attribute_id')->get();
-                                foreach($ProductVariantVariant as $productvariants){
-                                if($productvariants->attribute_terms['0']->attrterm_thumb != ''){
+                                    $ProductVariantVariant = \App\Models\ProductVariantVariant::with('attribute','attribute_terms')->where('estatus',1)->where('product_id',$Related->id)->groupBy('attribute_id')->get();
+                                    foreach($ProductVariantVariant as $productvariants){
+                                        if($productvariants->attribute_terms['0']->attrterm_thumb != ''){
+                                            ?>
+                                            <span class="wire_bangle_color mb-xxl-0 wire_bangle_color_img_part text-center wire_bangle_color_ring_part d-inline-block">
+                                                <div class="wire_bangle_color_part">
+                                                    <?php
+                                                        $product_attribute = \App\Models\ProductVariantVariant::with('attribute_terms','product_variant')->where('estatus',1)->where('attribute_id',$productvariants->attribute_id)->where('product_id',$Related->id)->groupBy('attribute_term_id')->get();
+                                                        $ia = 1;
+                                                        foreach($product_attribute as $attribute_term){
+                                                            $attributeurl =  URL('product-details/'.$attribute_term->product_variant->slug); 
+                                                            ?>
+                                                            <span class="form-check d-inline-block">
+                                                                <a href="{{ $attributeurl }}">
+                                                                <img src="{{ url('images/attrTermThumb/'.$attribute_term->attribute_terms[0]->attrterm_thumb) }}" alt="{{ $attribute_term->attribute_terms[0]->attrterm_name }}"  class="wire_bangle_color_img pe-auto">
+                                                                </a>
+                                                                <div class="wire_bangle_color_input_label"></div>
+                                                            </span>
+                                                            <?php        
+                                                            $ia++;    
+                                                        }
+                                                    ?>
+                                                </div>
+                                            </span>
+                                            <?php
+                                        } 
+                                    }
                                 ?>
-                                <span class="wire_bangle_color mb-xxl-0 wire_bangle_color_img_part text-center wire_bangle_color_ring_part d-inline-block"><div class="wire_bangle_color_part">
-                                <?php
-                                    $product_attribute = \App\Models\ProductVariantVariant::with('attribute_terms','product_variant')->where('estatus',1)->where('attribute_id',$productvariants->attribute_id)->where('product_id',$Related->id)->groupBy('attribute_term_id')->get();
-                                    $ia = 1;
-                                    foreach($product_attribute as $attribute_term){
-                                        $attributeurl =  URL('product-details/'.$attribute_term->product_variant->slug); 
-                                     ?>
-                                    <span class="form-check d-inline-block">
-                                        <a href="{{ $attributeurl }}">
-                                        <img src="{{ url('images/attrTermThumb/'.$attribute_term->attribute_terms[0]->attrterm_thumb) }}" alt="{{ $attribute_term->attribute_terms[0]->attrterm_name }}"  class="wire_bangle_color_img pe-auto">
-                                        </a>
-                                        <div class="wire_bangle_color_input_label"></div>
+                                <div class="wire_bangle_heading mb-2">
+                                    {{ $Related->primary_category->category_name }}
+                                    <input type="hidden" class="variant_id" value="{{ $Related->product_variant[$key]->id }}">    
+                                    <input type="hidden" class="item_type" value="0">    
+                                    <span type="button" class="btn btn-default add-to-wishlist-btn" data-toggle="tooltip" data-placement="right" title="Wishlist">
+                                        <?php 
+                                        if(is_wishlist($Related->product_variant[$key]->id,0)){ 
+                                            ?>
+                                            <i class="fas fa-heart heart-icon-part"></i>
+                                            <?php 
+                                        } else { 
+                                            ?>
+                                            <i class="far fa-heart"></i> 
+                                            <?php 
+                                        }
+                                        ?>
                                     </span>
-                                <?php        
-                                    $ia++;    
-                                }
-                                ?>
-                                </div></span>
-                                <?php
-                                    } 
-                                }
-                                ?>
-                               
+                                </div>
+                                <div class="wire_bangle_sub_heading wire_bangle_description"><a href="{{ $url }}">{{ $Related->product_title }}</a></div>
+                                <div class="d-flex justify-content-between  align-items-center">
+                                    <div class="d-flex align-items-center">
+                                        <span class="wire_bangle_price wire_bangle_price_part">
+                                            $ {{ $sale_price }}
+                                        </span>
+                                        <span class="ms-2 wire_bangle_dublicate_price product_detail_regular_price">$<span class="regular_price">{{ $regular_price }}</span></span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -89,4 +94,4 @@
             </div>
         </div>
     </div>
-    @endif
+@endif
