4267d90fa616297a3617fe85eebbfd345ba3ae72
Krushnkant Vadadoriya <39893686+Krushnkant@users.noreply.github.com>
2023-02-22 17:06:15 +0530

    KK

--- public/frontend/js/custom.js
+++ public/frontend/js/custom.js
@@ -751,8 +751,8 @@ $(document).on("click", "#cust_btn", function() {
 $(window).on('load', function() { // makes sure the whole site is loaded 
     $('body').css({ 'overflow': 'hidden' });
     $('.loader-btn').fadeOut(); // will first fade out the loading animation 
-    $('.header-loader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-    $('body').delay(350).css({ 'overflow': 'visible' });
+    $('.header-loader').delay(1000).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+    $('body').delay(1000).css({ 'overflow': 'visible' });
 });
 
 $(function() {
