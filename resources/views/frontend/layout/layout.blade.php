f09091d86a1c789f920e19657ce8d6d3e99fd423
Zahid <zahid.shaikh.9013@gmail.com>
2023-03-03 18:31:57 +0530

    working 03-03-2023 4

--- resources/views/frontend/layout/layout.blade.php
+++ resources/views/frontend/layout/layout.blade.php
@@ -52,6 +52,14 @@ $settings = \App\Models\Settings::first();
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-K6JH68V');</script>
     <!-- End Google Tag Manager -->
+
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "g2tcy46q6v");
+    </script>
     
 </head>
 <body>
