fb8f326fdadf82efabb05e3a316eba7e79f1b5ba
Zahid <zahid.shaikh.9013@gmail.com>
2023-03-07 16:58:29 +0530

    working 07-03-2023 11

--- app/View/Components/RelatedProduct.php
+++ app/View/Components/RelatedProduct.php
@@ -35,7 +35,7 @@ class RelatedProduct extends Component
     public function render()
     {
         $primary_category_idss = $this->categoryidss;
-        $Category = Category::select('slug')->whereIn(['primary_category_id' => $primary_category_idss])->first();
+        $Category = Category::select('slug')->whereIn(['id' => $primary_category_idss])->first();
         $Cslug = $Category->slug;
         $iscustom = $this->iscustom;
         //$ProductRelated= Product::select('products.id','products.product_title','products.primary_category_id','product_variants.slug','product_variants.alt_text','product_variants.images','product_variants.regular_price','product_variants.sale_price','product_variants.id as variant_id')->leftJoin("product_variants", "product_variants.product_id", "=", "products.id")->where(['products.is_custom' => 0,'products.estatus' => 1,'product_variants.estatus' => 1,'product_variants.term_item_id' => 2])->WhereIn('primary_category_id',$primary_category_idss)->where('products.id','<>',$this->productid)->groupBy('products.id')->limit(8)->get();
