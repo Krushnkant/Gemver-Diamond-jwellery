cc8144dc2592fe684f519e2531a81506337c1a50
Zahid <pankajahir631@gmail.com>
2023-03-24 09:30:13 +0530

    chnage 24-03-2023

--- app/Console/Commands/DiamondCron.php
+++ app/Console/Commands/DiamondCron.php
@@ -45,7 +45,7 @@ class DiamondCron extends Command
     {
         
         set_time_limit(0);
-       // \Log::info("Diamond Round Heart Cushion Uploaded!");
+        \Log::info("Diamond Round Heart Cushion Uploaded start !");
         $oldids = Diamond::whereIn('Shape',['Round','Heart','Cushion'])->get()->pluck('diamond_id')->toarray();
         // $public_path = __DIR__ . '/../../../public/csv/vdb_LG_diamonds.csv';
         // Excel::import(new ImportDiamondNewLatest, $public_path);
@@ -520,7 +520,7 @@ class DiamondCron extends Command
                 'StockStatus' => '0'
                 ]);
         }
-       
+        \Log::info("Diamond Round Heart Cushion Uploaded end !");
     }
 
     public function createSlug($title, $id = 0)
