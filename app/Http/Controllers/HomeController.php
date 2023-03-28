6c7564e1c14ca23193ebfbc3f11ebc2079a45f7b
Zahid <pankajahir631@gmail.com>
2023-03-28 15:41:47 +0530

    chnage 28-03-2023 8

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,7 +20,7 @@ class HomeController extends Controller
 {
     public function index(){
         
-
+        
         $fields = "id,caption,media_type,media_url,permalink,thumbnail_url,timestamp,username";
         $token = env('INSTAGRAM_TOKEN', '');
         $limit = 10;
@@ -43,7 +43,70 @@ class HomeController extends Controller
     }
 
     public function index1(){
-        
+       
+        ini_set('max_execution_time', 0);
+        ini_set('memory_limit', '1024M');
+        //Initial settings, Just specify Source and Destination Image folder.
+        $ImagesDirectory    = public_path('images/ProductImg/'); //Source Image Directory End with Slash
+        $DestImagesDirectory    = public_path('images/ProductImg1/'); //Destination Image Directory End with Slash
+        $NewImageWidth      = 850; //New Width of Image
+        $NewImageHeight     = 850; // New Height of Image
+        $Quality        = 80; //Image Quality
+       // dd($ImagesDirectory);
+        //Open Source Image directory, loop through each Image and resize it.
+        if($dir = opendir($ImagesDirectory)){
+            while(($file = readdir($dir))!== false){
+                $f = $file;
+                if(is_dir($file)){
+                    $subdir = opendir($ImagesDirectory.$file);
+                    while(($file1 = readdir($subdir))!== false){
+                        echo $imagePath = $ImagesDirectory.$f."/".$file1;
+                        echo"<br>";
+                        echo $destPath = $DestImagesDirectory.$f."/".$file1;
+                        echo"<br>";
+                        $checkValidImage = @getimagesize($imagePath);
+                        //	mkdir($destPath,2);
+                        if(file_exists($imagePath) && $checkValidImage) //Continue only if 2 given parameters are true
+                        {
+                            //Image looks valid, resize.
+                            if($this->resizeImage($imagePath,$destPath,$NewImageWidth,$NewImageHeight,$Quality))
+                            {
+                                echo $file.' resize Success!<br />'; 
+                                /*
+                                Now Image is resized, may be save information in database?
+                                */
+
+                            }else{
+                                echo $file.' resize Failed!<br />'; 
+                            }
+                        }
+                    }
+                }else{
+                    $imagePath = $ImagesDirectory.$file;
+                    $destPath = $DestImagesDirectory.$file;
+                    $checkValidImage = @getimagesize($imagePath);
+
+                    if(file_exists($imagePath) && $checkValidImage) //Continue only if 2 given parameters are true
+                    {
+                        //Image looks valid, resize.
+                        if($this->resizeImage($imagePath,$destPath,$NewImageWidth,$NewImageHeight,$Quality))
+                        {
+                            echo $file.' resize Success!<br />'; 
+                            /*
+                            Now Image is resized, may be save information in database?
+                            */
+
+                        }else{
+                            echo $file.' resize Failed!<br />';
+                        }
+                    }
+                }
+            }
+            closedir($dir);
+        }
+
+
+      
 
         
         // query the user media
@@ -221,6 +284,45 @@ class HomeController extends Controller
     public function sitemap(){
         return  response()->view('frontend.sitemap')->header('Content-Type', 'text/xml');
     }
+
+    //Function that resizes image.
+public function resizeImage($SrcImage,$DestImage, $MaxWidth,$MaxHeight,$Quality)
+{
+    list($iWidth,$iHeight,$type)    = getimagesize($SrcImage);
+    $ImageScale             = min($MaxWidth/$iWidth, $MaxHeight/$iHeight);
+    $NewWidth               = ceil($ImageScale*$iWidth);
+    $NewHeight              = ceil($ImageScale*$iHeight);
+    $NewCanves              = imagecreatetruecolor($NewWidth, $NewHeight);
+
+    switch(strtolower(image_type_to_mime_type($type)))
+    {
+        case 'image/jpeg':
+            $NewImage = imagecreatefromjpeg($SrcImage);
+            break;
+        case 'image/png':
+            $NewImage = imagecreatefrompng($SrcImage);
+            break;
+        case 'image/gif':
+            $NewImage = imagecreatefromgif($SrcImage);
+            break;
+        default:
+            return false;
+    }
+
+    // Resize Image
+    if(imagecopyresampled($NewCanves, $NewImage,0, 0, 0, 0, $NewWidth, $NewHeight, $iWidth, $iHeight))
+    {
+        // copy file
+        if(imagejpeg($NewCanves,$DestImage,$Quality))
+        {
+            imagedestroy($NewCanves);
+            return true;
+        }
+    }
+}
     
     
 }
+
+
+
