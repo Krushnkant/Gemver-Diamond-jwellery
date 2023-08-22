81c6808b3c2cd79b62f3ff90bd4b0713d7eeed08
Krushnkant Vadadoriya <39893686+Krushnkant@users.noreply.github.com>
2023-08-22 14:05:06 +0530

    KK
    Added Facebook Pixel Script to Header

--- resources/views/frontend/layout/layout.blade.php
+++ resources/views/frontend/layout/layout.blade.php
@@ -75,6 +75,22 @@ $settings = \App\Models\Settings::first();
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
         })(window, document, "clarity", "script", "g2tcy46q6v");
     </script>
+
+    <!-- Meta Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1935321983596004');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1935321983596004&ev=PageView&noscript=1"/></noscript>
+    <!-- End Meta Pixel Code -->
     
 </head>
 <body>
