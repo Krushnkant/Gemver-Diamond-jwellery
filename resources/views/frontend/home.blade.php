aa1bdd1ca870166a7167767bfa57a6e9804534c0
Krushnkant Vadadoriya <39893686+Krushnkant@users.noreply.github.com>
2023-08-23 11:29:32 +0530

    KK
    Google Tag Scripts, FB Meta Scripts & Tidio Chat Options Added

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -33,12 +33,31 @@ $settings = \App\Models\Settings::first();
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-962R43V393"></script>
     <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'AW-11131777521');
+    </script>
+    
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-962R43V393"></script>
+    
+    <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
     gtag('config', 'G-962R43V393');
     </script>
+
+    <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K6JH68V');</script>
+    <!-- End Google Tag Manager -->
 
     <script type="text/javascript">
         (function(c,l,a,r,i,t,y){
@@ -47,6 +66,22 @@ $settings = \App\Models\Settings::first();
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
         })(window, document, "clarity", "script", "g2tcy46q6v");
     </script>
+
+    <!-- Meta Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1935321983596004');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1935321983596004&ev=PageView&noscript=1"/></noscript>
+    <!-- End Meta Pixel Code -->
 </head>
 <body>
 
@@ -1276,6 +1311,7 @@ $settings = \App\Models\Settings::first();
 <script src="{{ asset('frontend/js/all.min.js') }}"></script>   
 <script src="{{ asset('frontend/js/jquery.cookie.min.js') }}"></script>   
 <script src="{{ asset('frontend/js/select2.min.js') }}"></script>   
+<script src="//code.tidio.co/tq3yyhojklue6eb308n5honxttekkw0y.js" async></script>
 
 <script>    
     $(document).ready(function(){
