27d842ac85d62e5cb23111966942b5fc71816094
pankaj5604 <100554658+pankaj5604@users.noreply.github.com>
2023-12-28 11:00:53 +0530

    change backend code

--- resources/views/components/countdown.blade.php
+++ resources/views/components/countdown.blade.php
@@ -8,47 +8,54 @@ $formattedDate = $date->format('jS F');
 $currentDateTime = Carbon::now();
 if($deal->start_date > $currentDateTime){
 ?>
-<ul class="nav countdown" style="background-color:  {{ $deal->background_color }}; color: {{ $deal->text_color }}; ">
-    <li style="margin-right: 50px;">
+<div class="" style="background-color:  {{ $deal->background_color }}; color: {{ $deal->text_color }}; ">
+    <div class="row countdown justify-content-center" >
+      <div class="col-12 col-md-6 col-lg-4 col-xl-3">
         <h5 class="digit">{{ $deal->title }}</h5>
-       
-    </li>
-    <li>
-        <h5 class="digit" id="days">00</h5>
-        <h6 class="text">
-			days
-        </h6>
-    </li>
-    <li>
-        <h5 class="digit" id="hours">00</h5>
-        <h6 class="text">
-            hours
-        </h6>
-    </li>
-    <li>
-        <h5 class="digit" id="mins">00</h5>
-        <h6 class="text">
-			minutes
-        </h6>
-    </li>
-    <li>
-        <h5 class="digit" id="secs">00</h5>
-        <h6 class="text">
-            seconds
-        </h6>
-    </li>
-
-    <li style="margin-left: 50px; margin-right: 20px; ">
+      </div>
+      <div class="col-12 col-md-6 col-lg-3 col-xl-3">
+        <ul class="nav-center">
+            
+            <li>
+                <h5 class="digit" id="days">00</h5>
+                <h6 class="text">
+                    days
+                </h6>
+            </li>
+            <li>
+                <h5 class="digit" id="hours">00</h5>
+                <h6 class="text">
+                    hours
+                </h6>
+            </li>
+            <li>
+                <h5 class="digit" id="mins">00</h5>
+                <h6 class="text">
+                    minutes
+                </h6>
+            </li>
+            <li>
+                <h5 class="digit" id="secs">00</h5>
+                <h6 class="text">
+                    seconds
+                </h6>
+            </li>
+    
+           
+        </ul>
+      </div>
+      <div class="col-12 col-md-6 col-lg-3 col-xl-2">
         <h5 class="digit" >{{ $deal->date_title }}</h5>
         <h6 class="text">
             {{ $formattedDate }}
         </h6>
-    </li>
-
-    <li style=" margin-top: 20px; ">
+      </div>
+      <div class="col-12 col-md-6 col-lg-2 col-xl-1">
         <a href="{{ $deal->url_button }}" target="_blank" style="color: {{ $deal->button_color }}; text-decoration: underline;  ">{{ $deal->text_button }}</a>
-    </li>
-</ul>
+      </div>
+    </div>
+</div>
+
 <?php } ?>
 <script>
     var count = new Date("{{ $deal->start_date }}").getTime();
