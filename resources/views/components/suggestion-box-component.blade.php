72a81027d71751f2ba42dce6ed76ef3ebaebfb97
Krushnkant Vadadoriya <krushnvadadoriya@gmail.com>
2024-06-12 11:06:12 +0530

    Update Suggestion Box

--- resources/views/components/suggestion-box-component.blade.php
+++ resources/views/components/suggestion-box-component.blade.php
@@ -1,16 +1,19 @@
-<div class="row" style="display: none;">
-    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
-    <div class="col-sm-6">
-        <img src="https://loosemoissanite.com/cdn/shop/files/Moissanite_experts.jpg?crop=center&height=666&v=1698664974&width=1000" />
-    </div>
-    <div class="col-sm-6">
-        <h5>Need a Suggestions?</h5>
-        <h5>Talk with professional</h5>
-        <p>Build trust by sharing your thoughts and discussing your views with our experts</p>
-            <div class="shop-colorful-bg-btn-div">
-                <a href="https://gemver.com/lab-diamond" class="shopnow_diamond">Shop Now <svg class="svg-inline--fa fa-arrow-right" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M438.6 278.6l-160 160C272.4 444.9 264.2 448 256 448s-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L338.8 288H32C14.33 288 .0016 273.7 .0016 256S14.33 224 32 224h306.8l-105.4-105.4c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160C451.1 245.9 451.1 266.1 438.6 278.6z"></path></svg><!-- <i class="fa fa-arrow-right"></i> Font Awesome fontawesome.com --></a>
-
-                <a href="https://gemver.com/lab-diamond" class="shopnow_diamond">Shop Now <svg class="svg-inline--fa fa-arrow-right" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M438.6 278.6l-160 160C272.4 444.9 264.2 448 256 448s-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L338.8 288H32C14.33 288 .0016 273.7 .0016 256S14.33 224 32 224h306.8l-105.4-105.4c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160C451.1 245.9 451.1 266.1 438.6 278.6z"></path></svg><!-- <i class="fa fa-arrow-right"></i> Font Awesome fontawesome.com --></a>
-            </div>
+<div class="suggestion-box-section" style="display: none; background-image: url(https://t4.ftcdn.net/jpg/02/92/56/91/360_F_292569116_Phht4uRj1YIuLFgBhrLu8171npBOcJcr.jpg); background-repeat: no-repeat; background-size: cover; padding: 160px 0; margin-bottom: 70px;">
+    <div class="container">
+        <div class="row" style="">
+            <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
+            <!-- <div class="col-sm-6">
+                <img src="https://loosemoissanite.com/cdn/shop/files/Moissanite_experts.jpg?crop=center&height=666&v=1698664974&width=1000" />
+            </div>-->
+            <div class="col-md-4 col-sm-12">
+                <h5 class="suggestion-sub-heading">Need Suggestions?</h5>
+                <h5 class="suggestion-heading">Talk with professional</h5>
+                <p class="suggestion-content">Build trust by sharing your thoughts and discussing your views with our experts</p>
+                <div class="shop-colorful-bg-btn-div">
+                    <a href="#" class="shopnow_diamond suggestion-btn">Chat with Us</a>
+                    <a href="#" class="shopnow_diamond suggestion-btn-outline">Email Us</a>
+                </div>
+            </div> 
+        </div>
     </div>
 </div>
